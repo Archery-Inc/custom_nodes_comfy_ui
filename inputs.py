267f83b39edab20301cd19921e82d18e3d9dc76a
root <root@tessellator>
2024-05-22 07:06:54 +0200

    add missing inputs

--- inputs.py
+++ inputs.py
@@ -62,6 +62,87 @@ class ArcheryInputAnimateDiffModelSelector:
         return (value,)
 
 
+WEIGHTS = ["linear", "ease in", "ease out", 'ease in-out', 'reverse in-out', 'weak input', 'weak output', 'weak middle', 'strong middle', 'style transfer', 'composition', 'strong style transfer']
+
+class ArcheryInputIpAdapterWeightTypeSelector:
+    CATEGORY = 'archery-inc'
+    RETURN_TYPES = (WEIGHTS,)
+    RETURN_NAMES = ("value",)
+    FUNCTION = "run"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"value": (WEIGHTS, ),}}
+
+    def run(self, value):
+        return (value,)
+
+
+class ArcheryInputControlNetSelector:
+    CATEGORY = 'archery-inc'
+    RETURN_TYPES = (folder_paths.get_filename_list("controlnet"),)
+    RETURN_NAMES = ("value",)
+    FUNCTION = "run"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"value": (folder_paths.get_filename_list("controlnet"), ),}}
+
+    def run(self, value):
+        return (value,)
+
+def vae_list():
+    vaes = folder_paths.get_filename_list("vae")
+    approx_vaes = folder_paths.get_filename_list("vae_approx")
+    sdxl_taesd_enc = False
+    sdxl_taesd_dec = False
+    sd1_taesd_enc = False
+    sd1_taesd_dec = False
+
+    for v in approx_vaes:
+        if v.startswith("taesd_decoder."):
+            sd1_taesd_dec = True
+        elif v.startswith("taesd_encoder."):
+            sd1_taesd_enc = True
+        elif v.startswith("taesdxl_decoder."):
+            sdxl_taesd_dec = True
+        elif v.startswith("taesdxl_encoder."):
+            sdxl_taesd_enc = True
+    if sd1_taesd_dec and sd1_taesd_enc:
+        vaes.append("taesd")
+    if sdxl_taesd_dec and sdxl_taesd_enc:
+        vaes.append("taesdxl")
+    return vaes
+
+
+class ArcheryInputVaeSelector:
+    CATEGORY = 'archery-inc'
+    RETURN_TYPES = (vae_list(),)
+    RETURN_NAMES = ("value",)
+    FUNCTION = "run"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"value": (vae_list(), ),}}
+
+    def run(self, value):
+        return (value,)
+
+
+class ArcheryInputUpscalerSelector:
+    CATEGORY = 'archery-inc'
+    RETURN_TYPES = (folder_paths.get_filename_list("upscale_models"),)
+    RETURN_NAMES = ("value",)
+    FUNCTION = "run"
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"value": (folder_paths.get_filename_list("upscale_models"), ),}}
+
+    def run(self, value):
+        return (value,)
+
+
 class ArcheryInputAnimateDiffMotionLoraSelector:
     CATEGORY = 'archery-inc'
     RETURN_TYPES = (folder_paths.get_filename_list("animatediff_motion_lora"),)
